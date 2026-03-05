8deaa0984621c7cedec68f8367754be9f9cf573a
Laura Nunes <lauracostanunes2@gmail.com>
2026-03-05 15:06:36 -0300

    Exercício 1

--- ClasseObjeto/src/Conta/Conta.java
+++ ClasseObjeto/src/Conta/Conta.java
@@ -1,56 +1,57 @@
+package Conta;
 public class Conta{
+    public int numeroConta, agencia;
     public String nomeCliente;
     public float saldo;
     public boolean status;
-    public int numeroConta, agencia;
 
-    public Conta(String nomeCliente, float saldo, boolean status, int numeroConta, int agencia) {
-        this.nomeCliente = nomeCliente;
+    public Conta(int numeroConta, int agencia, String nomeCliente){
         this.numeroConta = numeroConta;
         this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
         this.saldo = 0;
         this.status = true;
     }
 
-    public void depositar(float valor) {
-        if (this.status) {
+    public void depositar(float valor){
+        if (this.status){
             this.saldo += valor;
-            System.out.println("Depósito realizado");
-        }
-        else{
-            System.out.println("Conta inativa");
-        }
-    }
-
-    public void sacar(float valor) {
-        if (this.status && this.saldo >= valor) {
-            this.saldo -= valor;
-            System.out.println("Saque realizado. Saldo atual: R$" + this.saldo);
-        }
-        else if (!this.status) {
-            System.out.println("Conta inativa");
-        }
-        else {
-            System.out.println("Saldo insuficiente");
+            System.out.println("- Depósito realizado.");
+        } else{
+            System.out.println("* Conta inativa *");
         }
     }
 
-    public void encerrarConta() {
-        if (this.saldo == 0) {
+    public void sacar(float valor){
+        if (this.status){
+            if (this.saldo >= valor){
+                this.saldo -= valor;
+                System.out.println("- Saque realizado. Novo saldo: $" + this.saldo);
+            } else {
+                System.out.println("* Saldo indisponível *");
+            }
+        } else{
+            System.out.println("* Conta inativa *");
+        }
+    }
+
+    public void encerrar(){
+        if (this.status && this.saldo == 0){
             this.status = false;
-            System.out.println("Conta encerrada");
-        }
-        else {
-            System.out.println("Conta possui saldo");
+            System.out.println("- Conta encerrada.");
+        } else{
+            System.out.println("* Para encerrar a conta retire o saldo restante *");
         }
     }
 
-    // toString
-    public String toString() {
-        return "Cliente: " + this.nomeCliente +
-                " Número: " + this.numeroConta +
-                " Agência: " + this.agencia +
-                " Saldo: " + this.saldo +
-                " Status: " + this.status;
+    public String toString(){
+        return "--------------------------" +
+                "\nNúmero: " + this.numeroConta +
+                "\nAgência: " + this.agencia +
+                "\nCliente: " + this.nomeCliente +
+                "\nSaldo atual: " + this.saldo +
+                "\nStatus da conta: " + this.status +
+                "\n--------------------------";
     }
+
 }
